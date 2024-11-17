0081ef0ffc3e6a11efe65aea69eb6f738b712c1f
Nikolai Lehbrink <mail@nikolailehbr.ink>
2024-11-17 01:06:52 +0100

    feat: add required indicator to Label component

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps<{ required?: boolean }>()
+</script>
+
 <template>
-  <label class="text-sm font-bold text-neutral-900"><slot></slot></label>
+  <label class="text-sm font-bold text-neutral-900"
+    ><slot></slot>
+    <sup v-if="props.required" class="text-red-500 text-xs ml-0.5 -top-1">*</sup>
+  </label>
 </template>
