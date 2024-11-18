81d04c84650ff60280f540f0cdb8595b147712e0
Nikolai Lehbrink <nikolai.lehbrink@off-campers.com>
2024-11-18 15:20:18 +0100

    chore: add custom shadows to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'bottom-2': '0 2px 0 rgba(0, 0, 0, 0.1)',
+        'bottom-4': '0 4px 0 rgba(0, 0, 0, 0.1)',
+      },
+    },
   },
   plugins: [],
 }
